a74f4576a4e32b1430f5b2f787318e16da1c9818
cmkm <cmkm@lavabit.com>
2012-07-11 13:16:45 -0400

    improved animation with stop()

--- invthumbs.js
+++ invthumbs.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 
 	$("ul.invthumbs > li, ul.invthumbs2 > li").mouseenter(
     function() {
-      $(this).find("div.infopane").delay(100).fadeIn("slow");
+      $(this).find("div.infopane").stop(true,true).fadeIn();
     });
 
   $("ul.invthumbs > li, ul.invthumbs2 > li").mouseleave(
     function() {
-      $(this).find("div.infopane").delay(200).fadeOut("slow");
+      $(this).find("div.infopane").stop(true,true).delay(200).fadeOut();
     });
 
 });  

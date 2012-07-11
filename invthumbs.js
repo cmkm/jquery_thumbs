1c340516bee7cd4370078890016387f125705ce6
cmkm <cmkm@lavabit.com>
2012-07-11 09:38:46 -0400

    created git repo

---
+++ invthumbs.js
@@ -0,0 +1,9 @@
+  $("ul.invthumbs > li, ul.invthumbs2 > li").mouseenter(
+    function() {
+      $(this).find("div.infopane").delay(100).toggle("slow");
+    });
+
+  $("ul.invthumbs > li, ul.invthumbs2 > li").mouseleave(
+    function() {
+      $(this).find("div.infopane").delay(400).toggle("slow");
+    });

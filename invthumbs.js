17313a89092962cef546686fc4ca148341b6daed
cmkm <cmkm@lavabit.com>
2012-07-11 12:46:15 -0400

    change toggle to fadeIn/fadeOut

--- invthumbs.js
+++ invthumbs.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
 
+	var 
+
 	$("ul.invthumbs > li, ul.invthumbs2 > li").mouseenter(
     function() {
-      $(this).find("div.infopane").delay(100).toggle("slow");
+      $(this).find("div.infopane").delay(100).fadeIn("slow");
     });
 
   $("ul.invthumbs > li, ul.invthumbs2 > li").mouseleave(
     function() {
-      $(this).find("div.infopane").delay(400).toggle("slow");
+      $(this).find("div.infopane").delay(200).fadeOut("slow");
     });
 
 });  
